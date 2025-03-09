2c39eac2c184d050baeab2ba388ad7419289bd12
Aleksei <svid.aleksei@ya.ru>
2025-03-09 16:03:46 +0300

    modified tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,22 @@
 import pytest
 
-from src.main import Product
-from src.main import Category
+from src.main import Category, Product
 
 
 @pytest.fixture
 def test_case_product():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
+
 @pytest.fixture
 def test_product4():
     return {
-            "name": "Samsung Galaxy S23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5,
-        }
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+
 
 @pytest.fixture
 def test_case_category():
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,27 @@
-from src.main import Category, new_product, add_product
-from src.main import Product
+import pytest
+
+from src.main import Category
 
 
-def test_init_product(test_case_product):
+def test_init_product(test_case_product, test_product4):
     assert test_case_product.name == "Samsung Galaxy S23 Ultra"
     assert test_case_product.description == "256GB, Серый цвет, 200MP камера"
     assert test_case_product.price == 180000.0
     assert test_case_product.quantity == 5
+    new_product = test_case_product.new_product(test_product4)
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+    assert new_product.description == "256GB, Серый цвет, 200MP камера"
+    assert new_product.price == 180000.0
+    assert new_product.quantity == 5
+    new_product.price = 800
+    assert new_product.price == 800
+    with pytest.raises(ValueError):
+        new_product.price = -100
+    with pytest.raises(ValueError):
+        new_product.price = 0
 
 
-def test_init_category(test_case_category):
+def test_init_category(test_case_category, test_product4):
     assert test_case_category.name == "Смартфоны"
     assert (
         test_case_category.description
@@ -18,9 +30,14 @@ def test_init_category(test_case_category):
     assert test_case_category.products == []
     assert Category.category_count == 1
     assert Category.product_count == 0
-
-
-def test_add_product(test_case_product, test_product4):
-
-    assert test_init_category.add_product(test_product4) == 123
-
+    assert test_case_category.add_product(test_product4) is None
+    assert test_case_category.products == [
+        {
+            "description": "256GB, Серый цвет, 200MP камера",
+            "name": "Samsung Galaxy S23 Ultra",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    ]
+    assert Category.category_count == 1
+    assert Category.product_count == 1
