f5659bf10c7bc518ade0ff489a83457210a21a51
Aleksei <svid.aleksei@ya.ru>
2025-03-11 16:44:47 +0300

    Homework_15_1 update

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,11 @@ def test_case_category():
     return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [],
+        [
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
     )
 
 
